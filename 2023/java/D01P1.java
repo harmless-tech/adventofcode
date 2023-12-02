74330dff22baa946a51e801e93d0047d16d08a69
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-12-01 19:58:35 -0500

    Initial Commit

---
+++ 2023/java/D01P1.java
@@ -0,0 +1,5 @@
+public class D01P1 {
+    public static void main(String[] args) {
+        System.out.println("Day 1 - Part 1");
+    }
+}
---
+++ 2023/java/D01P2.java
@@ -0,0 +1,5 @@
+public class D01P2 {
+    public static void main(String[] args) {
+        System.out.println("Day 1 - Part 2");
+    }
+}
