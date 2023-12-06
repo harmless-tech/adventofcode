0228a23a6424417bed063c2bf73a960c6429b259
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-12-06 14:23:51 -0500

    Day 5 - Java

--- 2023/java/d05p2.java
+++ 2023/java/d05p2.java
@@ -3,11 +3,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class d05p1 {
+public class d05p2 {
     private static final int ZERO = 48;
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Day 5 - Part 1");
+        System.out.println("Day 5 - Part 2");
 
         BufferedReader reader = new BufferedReader(new FileReader("../_input/day05.txt"));
 
@@ -25,7 +25,7 @@ public class d05p1 {
         }
         String[] lines = linesArr.toArray(String[]::new);
 
-        long[] seeds = null;
+        ArrayList<Seed> seeds = null;
         ArrayList<Mappings[]> mappings = new ArrayList<>();
         ArrayList<String> mapLines = new ArrayList<>();
 
@@ -43,32 +43,68 @@ public class d05p1 {
 
         for (Mappings[] m : mappings) mapVals(seeds, m);
 
-        long min = seeds[0];
-        for (var s : seeds) min = Math.min(min, s);
+        long min = seeds.getFirst().start;
+        for (var s : seeds) min = Math.min(min, s.start);
         return min;
     }
 
-    private static void mapVals(long[] vals, Mappings[] maps) {
-        boolean[] changes = new boolean[vals.length];
+    private static void mapVals(ArrayList<Seed> vals, Mappings[] maps) {
+        ArrayList<Boolean> changes = new ArrayList<>();
+        for (int i = 0; i < vals.size(); i++) changes.add(false);
+
         for (var map : maps) {
             var destStart = map.destStart;
+            var destEnd = destStart + map.rangeLen - 1;
             var srcStart = map.srcStart;
             var srcEnd = srcStart + map.rangeLen - 1;
             var diff = destStart - srcStart;
 
-            for (int i = 0; i < vals.length; i++) {
-                var v = vals[i];
-                if (v >= srcStart && v <= srcEnd && !changes[i]) {
-                    vals[i] += diff;
-                    changes[i] = true;
+            for (int i = 0; i < vals.size(); i++) {
+                if (!changes.get(i)) {
+                    var val = vals.get(i);
+                    var v1 = val.start;
+                    var v2 = val.end;
+
+                    if (v1 >= srcStart && v2 <= srcEnd) {
+                        // If the range is completly within, then just shift it.
+                        var seed = vals.get(i);
+                        seed.start += diff;
+                        seed.end += diff;
+                        changes.set(i, true);
+                    } else if (v1 >= srcStart && v1 <= srcEnd && v2 > srcEnd) {
+                        // If the upper range is out of bounds
+                        var seed = vals.get(i);
+                        seed.start += diff;
+                        seed.end = destEnd;
+
+                        vals.add(new Seed(srcEnd + 1, v2));
+
+                        changes.set(i, true);
+                        changes.add(false);
+                    } else if (v1 < srcStart && v2 >= srcStart && v2 <= srcEnd) {
+                        // If the lower range is out of bounds
+                        var seed = vals.get(i);
+                        seed.start = destStart;
+                        seed.end += diff;
+
+                        vals.add(new Seed(v1, srcStart - 1));
+
+                        changes.set(i, true);
+                        changes.add(false);
+                    }
                 }
             }
         }
     }
 
-    private static long[] convertSeeds(String str) {
+    private static ArrayList<Seed> convertSeeds(String str) {
         String seedLine = str.split(":")[1].trim();
-        return getNums(seedLine);
+        long[] nums = getNums(seedLine);
+
+        ArrayList<Seed> seeds = new ArrayList<>();
+        for (int i = 0; i < nums.length; i += 2)
+            seeds.add(new Seed(nums[i], nums[i] + nums[i + 1] - 1));
+        return seeds;
     }
 
     private static Mappings[] convertMap(String[] items) {
@@ -97,6 +133,16 @@ public class d05p1 {
             acc += (long) (Math.pow(10.0, str.length() - i - 1) * (c - ZERO));
         }
         return acc;
+    }
+
+    private static class Seed {
+        public long start;
+        public long end;
+
+        public Seed(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
     }
 
     private record Mappings(long destStart, long srcStart, long rangeLen) {}
