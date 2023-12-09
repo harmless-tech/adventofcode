5f67e19cf6584a983e9c2d727371bc1ae3c7a46b
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-12-09 01:43:57 -0500

    Day 9 - Rust

---
+++ 2023/rust/src/bin/d09p2.rs
@@ -0,0 +1,75 @@
+use std::fs;
+
+fn main() {
+    println!("Day 9 - Part 2");
+
+    let str = fs::read_to_string("../_input/day09.txt").unwrap();
+    let lines: Vec<&str> = str.lines().collect();
+
+    let total = process(lines);
+    println!("Total: {}", total);
+}
+
+fn process(lines: Vec<&str>) -> i32 {
+    let readings: Vec<Vec<i32>> = lines
+        .iter()
+        .map(|s| s.split_whitespace().map(str_to_i32).collect())
+        .collect();
+
+    let mut total = 0;
+    for reading in readings {
+        let mut stack = vec![reading];
+        let mut i = 0;
+        while !stack[i].iter().all(|e| *e == 0) {
+            let v = stack[i].windows(2).map(|w| w[1] - w[0]).collect();
+            stack.push(v);
+            i += 1;
+        }
+
+        stack.reverse();
+        stack[0].insert(0, 0);
+
+        let mut i = 1;
+        while i < stack.len() {
+            let a = {
+                let a = stack.get(i - 1).unwrap();
+                *a.first().unwrap()
+            };
+            let b = {
+                let b = stack.get(i).unwrap();
+                *b.first().unwrap()
+            };
+
+            stack.get_mut(i).unwrap().insert(0, b - a);
+
+            i += 1;
+        }
+
+        total += stack.last().unwrap().first().unwrap();
+    }
+
+    total
+}
+
+fn str_to_i32(str: &str) -> i32 {
+    str.parse().unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::process;
+
+    #[test]
+    fn test() {
+        assert_eq!(
+            process(
+                r"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
+                    .split('\n')
+                    .collect()
+            ),
+            2
+        );
+    }
+}
