58ca5d0be149ca67fa15200f22cc7edde6060096
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-12-09 23:43:15 -0500

    Day 9 - Swift

---
+++ 2023/swift/d09p2.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+func main() {
+    print("Day 9 - Part 2")
+
+    let raw_data = try! NSString(
+        contentsOfFile: "../_input/day09.txt", encoding: String.Encoding.utf8.rawValue)
+    let data = raw_data as String
+
+    let total = process(String(data))
+    print("Total: \(total)")
+}
+
+func process(_ string: String) -> Int {
+    let readings: [[Int]] = string.split(separator: "\n").map({
+        $0.components(separatedBy: " ").map({ Int($0)! })
+    })
+
+    var total = 0
+    for reading in readings {
+        var stack = [reading]
+
+        var i = 0
+        while !stack[i].allSatisfy({ $0 == 0 }) {
+            let s = stack[i]
+            var v: [Int] = []
+            var vi = 0
+
+            while vi < s.endIndex - 1 {
+                v.append(s[vi + 1] - s[vi])
+                vi += 1
+            }
+
+            stack.append(v)
+            i += 1
+        }
+
+        stack.reverse()
+        stack[0].insert(0, at: 0)
+
+        i = 1
+        while i < stack.count {
+            let a = stack[i - 1].first!
+            let b = stack[i].first!
+
+            stack[i].insert(b - a, at: 0)
+            i += 1
+        }
+
+        total += stack.last!.first!
+    }
+
+    return total
+}
+
+main()
